704e9780e0b938caa33a794fbe0f9ff8230ff765
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-12-06 12:40:58 +0300

    --amend

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getProfil, login, register } from "../controllers/auth.controller.js";
+import {
+  getProfil,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/logout", logout);
 
 router.get("/profil", authMiddleware, getProfil);
 
