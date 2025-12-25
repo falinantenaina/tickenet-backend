0a0ade1227129f92a9e2b367b90dc0d4ecb894db
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-12-25 19:06:53 +0300

    Update cookie configuration

--- config/generateTokenAndSetCookie.js
+++ config/generateTokenAndSetCookie.js
@@ -9,6 +9,5 @@ export const generateTokenAndSetCookie = (userId, res) => {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24,
     secure: process.env.NODE_ENV == "production",
-    sameSite: "none",
   });
 };
